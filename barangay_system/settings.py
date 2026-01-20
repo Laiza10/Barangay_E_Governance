de8757e68093969a4ac44879cb2e67a7a5608c75
Laiza Concepcion <laizaconcepcionbsit@gmail.com>
2026-01-20 23:19:00 +0800

    Configure settings for deployment on PythonAnywhere

--- barangay_system/settings.py
+++ barangay_system/settings.py
@@ -11,25 +11,29 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# ----------------------
+# Build paths
+# ----------------------
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# ----------------------
+# Security
+# ----------------------
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0xoxobp7sdquso(=z5cf2dt)^&anao4k(85zwgoyw%!ghcub=q'
+# SECRET_KEY for development; use environment variable in production
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-0xoxobp7sdquso(=z5cf2dt)^&anao4k(85zwgoyw%!ghcub=q')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# Only allow PythonAnywhere domain
+ALLOWED_HOSTS = ['Laiza1021.pythonanywhere.com']
 
-
+# ----------------------
 # Application definition
-
+# ----------------------
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,10 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Your apps
     'core',
     'widget_tweaks',
 ]
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -58,7 +62,7 @@ ROOT_URLCONF = 'barangay_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],  # global templates folder
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,10 +76,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'barangay_system.wsgi.application'
 
-
+# ----------------------
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
+# ----------------------
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -83,15 +86,9 @@ DATABASES = {
     }
 }
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
+# ----------------------
 # Password validation
-# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
-
+# ----------------------
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -107,20 +104,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+# ----------------------
 # Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
-
+# ----------------------
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
+# ----------------------
+# Static files
+# ----------------------
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # for collectstatic
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
-STATIC_URL = 'static/'
+# ----------------------
+# Default primary key field type
+# ----------------------
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
